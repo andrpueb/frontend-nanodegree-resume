1856b60492de5728fcb44a576f1b00ac9c8e3df0
Andres Puentes <andresf_puentes@hotmail.com>
2016-11-08 20:49:33 +0000

    Added json to builder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,93 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var work = {
+   jobs : [
+     {
+       "employer": "Kaplan International",
+       "title": "Sales Executive",
+       "location": "London",
+       "dates": "Apr 2015 - Apr 2016",
+       "description": "Worked as a sales consultant for one of the greates english schools in the world, gave outstanding results durying all the time working there"
+     },{
+       "employer": "Kumon Institute of education",
+       "title": "Franchise Coordinator",
+       "location": "Bogota",
+       "dates": "July 2013 - Dec 2013",
+       "description": "Amazing company dedicated to make the learning of maths and languages something enjoyable for kids"
+     }
+   ]
+
+  };
+
+ var projects = {
+   projects : [
+     {
+       "title" : "ceramind.co",
+       "dates" : "2016",
+       "description" : "Website created for a company dedicated to produce refractory ceramic products",
+       "images": [
+
+       ]
+     },{
+       "title" : "holisticmind.com",
+       "dates" : "2016",
+       "description" : "Creation for helthy nutrition company located in london",
+       "images" : [
+
+       ]
+     }
+   ]
+ };
+
+ var bio = {
+   "name" : "Andres Puentes",
+   "role" : "Front End Web Developer",
+   "welcomeMessage" : "Hi There, I am Andres Puentes and I am ready to help you to build your website",
+   "biopic" : "images/me.jpg",
+   "contacts" : {
+     "mobile" : "+447864968719",
+     "email" : "andresfpuentes@gmail.com",
+     "github" : "andrpueb",
+     "twitter" : "andrpueb",
+     "location" : "London, UK"
+   },
+   "skills" : ["github", "gulp", "css", "html", "flexbox"]
+ }
+
+ var education = {
+   "schools" : [
+     {
+       "name" : "Escuela Colombiana de Ingenieria",
+       "city" : "Bogota, Colombia",
+       "major" : "Electronic Engineering",
+       "graduationYear" : "2007"
+   },
+   {
+       "name" : "Politecnico Grancolombiano",
+       "city" : "Bogota, Colombia",
+       "major" : "International Business",
+       "graduationYear" : "2012"
+     }
+
+   ],
+   "onlineCourses" : [
+     {
+       "title" : "Front End Web Developer",
+       "school" : "CodeAcademy",
+       "dates" : "2016",
+       "url" : "https://www.codecademy.com/"
+     },{
+       "title" : "Front End Web Developer",
+       "school" : "FreeCodeCamp",
+       "dates" : "2016",
+       "url" : "https://www.freecodecamp.com/"
+     },{
+       "title" : "Front End Web Developer Nanodegree",
+       "school" : "Udacity",
+       "dates" : "2016",
+       "url" : "https://www.udacity.com/"
+     }
+   ]
+ };
