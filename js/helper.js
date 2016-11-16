91dcd86a2f356fdaa61f0661a9e9219fd4543f10
Andres Puentes <andresf_puentes@hotmail.com>
2016-11-16 15:51:31 +0000

    Added images in projects, backgrounds and logos in skills

--- js/helper.js
+++ js/helper.js
@@ -15,19 +15,19 @@ replace the %data% placeholder text you see in them.
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="social-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item entypo-mobile"><span class="orange-text"></span><span class="social-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item entypo-mail"><span class="orange-text"></span><span class="social-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item zocial-twitter"><span class="orange-text"></span><span class="social-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item entypo-github"><span class="orange-text"></span><span class="social-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="orange-text"></span><span class="social-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text"></span><span class="social-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-row"></ul>';
+var HTMLskills = '<li class="flex-item skill"><div class="skillWrapper"><a  href="#" data-toggle="tooltip" data-placement="bottom" title="%data%" class="skillTip" ><img class="skillLogo" src="images/%data%.svg" alt="%data%" /></a></div></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
@@ -86,7 +86,7 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  console.log("pageX: " + loc.pageX + " " + "pageY: " + loc.pageY);
 });
 
 
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,49 +2,11 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- var work = {
-   jobs : [
-     {
-       "employer": "Kaplan International",
-       "title": "Sales Executive",
-       "location": "London",
-       "dates": "Apr 2015 - Apr 2016",
-       "description": "Worked as a sales consultant for one of the greates english schools in the world, gave outstanding results durying all the time working there"
-     },{
-       "employer": "Kumon Institute of education",
-       "title": "Franchise Coordinator",
-       "location": "Bogota",
-       "dates": "July 2013 - Dec 2013",
-       "description": "Amazing company dedicated to make the learning of maths and languages something enjoyable for kids"
-     }
-   ]
-
-  };
-
- var projects = {
-   projects : [
-     {
-       "title" : "ceramind.co",
-       "dates" : "2016",
-       "description" : "Website created for a company dedicated to produce refractory ceramic products",
-       "images": [
-
-       ]
-     },{
-       "title" : "holisticmind.com",
-       "dates" : "2016",
-       "description" : "Creation for helthy nutrition company located in london",
-       "images" : [
-
-       ]
-     }
-   ]
- };
 
  var bio = {
    "name" : "Andres Puentes",
    "role" : "Front End Web Developer",
-   "welcomeMessage" : "Hi There, I am Andres Puentes and I am ready to help you to build your website",
+   "welcomeMessage" : "Hi! I am Andres Puentes, a London based Junior Web Developer who has been learning all the magic of web development for the past 2 years, I am here ",
    "biopic" : "images/me.jpg",
    "contacts" : {
      "mobile" : "+447864968719",
@@ -53,8 +15,8 @@ This is empty on purpose! Your code to build the resume will go here.
      "twitter" : "andrpueb",
      "location" : "London, UK"
    },
-   "skills" : ["github", "gulp", "css", "html", "flexbox"]
- }
+   "skills" : [ "css3", "html5", "javascript", "jquery", "bootstrap", "git", "gulp"]
+ };
 
  var education = {
    "schools" : [
@@ -91,3 +53,148 @@ This is empty on purpose! Your code to build the resume will go here.
      }
    ]
  };
+
+ var work = {
+   jobs : [
+     {
+       "employer": "Kaplan International",
+       "title": "Sales Executive",
+       "location": "London",
+       "dates": "Apr 2015 - Apr 2016",
+       "description": "Worked as a sales executive for one of the greates english schools in the world, Providing outstanding service to potential customers in South America via email or phone in order to convert those leads into sales while interacting with other departments regarding the sales process and documentation (finance, customer care, compliance.)"
+     },{
+       "employer": "Kumon Institute of education",
+       "title": "Franchise Coordinator",
+       "location": "Bogota",
+       "dates": "July 2013 - Dec 2013",
+       "description": "Amazing company dedicated to make the learning of maths and languages something enjoyable for kids. Worked Auditing the franchises that were under my supervision checking the performance and execution of the guidelines sent from the headquarters, analysing figures and giving term results. Responsible of implementing and improving processes by delivering trainings and workshops."
+     }
+   ]
+
+  };
+
+ var projects = {
+   projects : [
+     {
+       "title" : "ceramind.co",
+       "dates" : "2016",
+       "description" : "This was my first project, is a website created for a company dedicated to produce refractory ceramic products, was created using HTML, CSS and basic PHP",
+       "images": ["images/project1.jpg", "images/project2.jpg"]
+     },{
+       "title" : "holisticmind.com",
+       "dates" : "2016",
+       "description" : "I am now working on this big project, is an online shop of natural products and a place were people can meet to share their thoughts about healthy living",
+       "images" : ["images/yoga1.jpg", "images/yoga2.jpg"]
+     }
+   ]
+ };
+
+
+
+
+
+ var formattedName = HTMLheaderName.replace("%data%", bio.name);
+ var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+ var formattedEducationName = HTMLschoolName.replace("%data%", education.lastSchool);
+ var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
+ var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+ var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+ var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+ var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+ var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
+ $("#header").append(formattedPic);
+ $("#header").append(formattedMessage);
+ $("#topContacts").append(formattedMobile);
+ $("#topContacts").append(formattedEmail);
+ $("#topContacts").append(formattedTwitter);
+ $("#topContacts").append(formattedGithub);
+
+
+bio.display = function(){
+  $("#header").append(HTMLskillsStart);
+  bio.skills.forEach(function(skill){
+   var formattedSkills = HTMLskills.replace("%data%", skill).replace("%data%", skill).replace("%data%", skill).replace("%data%", skill);
+   $("#skills").append(formattedSkills);
+ })
+ };
+
+ bio.display();
+
+   work.display = function(){
+     work.jobs.forEach(function(job){
+       $("#workExperience").append(HTMLworkStart);
+       var formattedEmployer = HTMLworkEmployer.replace("%data%", job.employer);
+       var formattedTitle = HTMLworkTitle.replace("%data%", job.title);
+       var formattedDate = HTMLworkDates.replace("%data%" , job.dates);
+       var formattedLocation = HTMLworkLocation.replace("%data%" , job.location);
+       var formattedDescription = HTMLworkDescription.replace("%data%" , job.description)
+       var finalEmployerTitle = formattedEmployer + formattedTitle;
+       $(".work-entry:last").append(finalEmployerTitle, formattedDate, formattedLocation, formattedDescription);
+     })
+     };
+
+     work.display();
+
+     $("#main").append(internationalizeButton);
+
+     projects.display = function(){
+       projects.projects.forEach(function(project){
+         $("#projects").append(HTMLprojectStart);
+         var formattedProTitle = HTMLprojectTitle.replace("%data%" , project.title);
+         var formattedProjectDates = HTMLprojectDates.replace("%data%" , project.dates);
+         var formattedProjectDescription = HTMLprojectDescription.replace("%data%" , project.description);
+         $(".project-entry:last").append(formattedProTitle, formattedProjectDates, formattedProjectDescription);
+         project.images.forEach(function(image){
+           var formattedProjectImage = HTMLprojectImage.replace("%data%", image)
+           $(".project-entry:last").append(formattedProjectImage);
+         })
+
+
+       })
+     };
+
+     projects.display();
+
+     education.display = function(){
+       education.schools.forEach(function(school){
+         $("#education").append(HTMLschoolStart);
+         var formattedSchoolName = HTMLschoolName.replace("%data%" , school.name);
+         var formattedSchoolDegree = HTMLschoolDegree.replace("%data%" , school.major);
+         var formattedSchoolDate = HTMLschoolDates.replace("%data%" , school.graduationYear);
+         var formatteSchoolCity = HTMLschoolLocation.replace("%data%" , school.city)
+         $(".education-entry:last").append(formattedSchoolName, formattedSchoolDegree, formattedSchoolDate, formatteSchoolCity);
+       })
+     };
+
+     education.display();
+
+
+     education.onlineCourses.display = function(){
+       $('#education').append(HTMLonlineClasses);
+       for (var i=0; i < education.onlineCourses.length; i++){
+         var formattedOnlineTitle = HTMLonlineTitle.replace("%data%" , education.onlineCourses[i].title);;
+         var formattedOnlineSchool = HTMLonlineSchool.replace("%data%" , education.onlineCourses[i].school);
+         var formattedOnlineDates = HTMLonlineDates.replace("%data%" , education.onlineCourses[i].dates);
+         var formattedOnlineUrl = HTMLonlineURL.replace("%data%" , education.onlineCourses[i].url)
+         $("#education").append(formattedOnlineTitle, formattedOnlineSchool, formattedOnlineDates, formattedOnlineUrl);
+       }
+     }
+     education.onlineCourses.display();
+
+
+ $("#mapDiv").append(googleMap);
+
+
+ //Changes to the code from here
+$('span:first').addClass('role');
+
+$('[data-toggle="tooltip"]').tooltip();
+
+$('.skillLogo').hover(function(){
+  $(this).addClass('hoverSkill')
+}, function(){
+  $('.skillLogo').removeClass('hoverSkill')
+});
